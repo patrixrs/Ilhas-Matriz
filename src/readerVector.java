aade6c53d3dab06e497e821f3d2611f06ddadafd
patrixrs <patrix.pfr@gmail.com>
2017-04-15 00:49:20 -0300

    Correções

--- src/readerVector.java
+++ src/readerVector.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
 public class readerVector {
 	static String padrao;
-	static int x, y, f, g, resp, cont, indX, indY;
+	static int x, y, f, g;
 	static int[][] vetor;
 
 	public int[][] reader() {
 		try {
-			Scanner file = new Scanner(new File("src/Arquivos/example_3.txt")); 
+			Scanner file = new Scanner(new File("src/Arquivos/example_2.txt"));
 			x = file.nextInt(); // recebem os primeiros parametros inteiros
 			y = file.nextInt();
-			vetor = new int[x][y];			
+			vetor = new int[x][y];
 			padrao = file.nextLine();
 			while (file.hasNext()) {
-				for (int i = 0; i < x; i++) {
-					padrao = file.nextLine(); // recebe a linha como string					
+				for (int i = 0; i < y; i++) {
+					padrao = file.nextLine(); // recebe a linha como string
 					f = 0;
 					g = 1;
-					for (int j = 0; j < y; j++) {
+					for (int j = 0; j < x; j++) {
 						int valor = Integer.parseInt(padrao.substring(f, g));
-						vetor[j][i] = valor;						
+						vetor[j][i] = valor;
 						f++;
 						g++;
 					}
 				}
 			}
-			file.close();			
+			file.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -34,4 +34,11 @@ public class readerVector {
 		return vetor;
 	}
 
+	public int largura() {
+		return y;
+	}
+
+	public int altura() {
+		return x;
+	}
 }
